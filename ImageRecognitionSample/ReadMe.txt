e49d0632ccedcffbbb46c18deec56aff6404d5bf
saigyoji205 <saigyoji205@gmail.com>
2014-10-31 16:29:55 +0900

    Commit ImageRecognitionSample

---
+++ ImageRecognitionSample/ReadMe.txt
@@ -0,0 +1,6 @@
+サンプルアプリの使用にあたって
+
+・APIキーの設定方法について
+　APIキーは src/jp/ne/docomo/smt/dev/imagerecognition/sample/RecognitionActivity.java 内の
+　APIKEY に記載してください。
+
---
+++ ImageRecognitionSample/gen/jp/ne/docomo/smt/dev/imagerecognition/sample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jp.ne.docomo.smt.dev.imagerecognition.sample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
